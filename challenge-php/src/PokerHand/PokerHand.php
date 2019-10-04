bc66de2d8fa11433e0c55644c736f3055a7622bc
Zach Bajaber <zachariab@gmail.com>
2019-10-04 09:18:21 -0600

    Add complete PHP challenge

--- challenge-php/src/PokerHand/PokerHand.php
+++ challenge-php/src/PokerHand/PokerHand.php
@@ -4,13 +4,107 @@ namespace PokerHand;
 
 class PokerHand
 {
+
+    public $hand;
+    private $cardRanks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A'];
+    private $cardSuits = ['s', 'c', 'd', 'h'];
+    private $maxRankValue = 12;
+
+    // Accept a string of cards, validate and then store as an array in $this->hand
     public function __construct($hand)
     {
+        // Convert '10' to 'T' so card ranks are uniformly 1 character
+        $this->hand = str_replace("10", "T", $hand);
+        $this->hand = explode(" ", $this->hand);
+        // Cards should be unique
+        $this->hand = array_unique($this->hand);
+        // 5 unique cards are required
+        if (count($this->hand) != 5) {
+            throw new \InvalidArgumentException('PokerHand requires 5 unique cards in input string. Hand contained '. count($this->hand) . 'cards');
+        }
+        // Validate card strings
+        foreach ($this->hand as &$card) {
+            if (preg_match("/[2-9TJQKA][hcsd]/", $card) === 0)
+                throw new \InvalidArgumentException('Invalid card: ' . $card);
+        }
     }
 
+    // Main public function of class, optimized for readabilty over speed
     public function getRank()
     {
-        // TODO: Implement poker hand ranking
-        return 'Royal Flush';
+        $rankFrequncies = $this->getRankFrequencies();
+
+        if ($this->hasFlush() && $this->hasStraight() && $this->getHandHighRankValue() == $this->maxRankValue)
+            return 'Royal Flush';
+        if ($this->hasFlush() && $this->hasStraight())
+            return 'Straight Flush';
+        if (in_array(4, $rankFrequncies))
+            return 'Four of a Kind';
+        if ($rankFrequncies == [2,3])
+            return 'Full House';
+        if ($this->hasFlush())
+            return 'Flush';
+        if ($this->hasStraight())
+            return 'Straight';
+        if ($rankFrequncies == [1,1,3])
+            return 'Three of a Kind';
+        if ($rankFrequncies == [1,2,2])
+            return 'Two Pair';
+        if ($rankFrequncies == [1,1,1,2]) 
+            return 'One Pair';
+
+        return "High Card";
     }
+    
+    // Return an array of suit => number of cards in the suit
+    private function getSuitFrequencies() {
+        $suits = array_fill_keys($this->cardSuits, 0);
+        foreach ($this->hand as &$card) { 
+            $suits[str_split($card)[1]]++;   
+        }
+        return $suits;
+    }
+
+    // Get the highest numerical rank in the hand
+    private function getHandHighRankValue() {
+        $highCard = -1;
+        foreach ($this->hand as &$card) { 
+            $rank = array_search(str_split($card)[0], $this->cardRanks);
+            if ($rank > $highCard) {
+                $highCard = $rank;
+            }       
+        }
+        return $highCard;
+    }
+
+    // Return true if hand contains 5 of the same suit
+    private function hasFlush() {
+        foreach($this->getSuitFrequencies() as $suit => $suitCount) {
+            if ($suitCount == 5)
+                return true;
+        }
+        return false;
+    }
+    
+    // Return true if hand contains 5 ranks in sequence
+    private function hasStraight() {
+        $rankTotals = array_fill_keys($this->cardRanks, 0);
+        foreach ($this->hand as &$card) { 
+            $rankTotals[str_split($card)[0]]++;   
+        }
+        $hasStraight = strpos(implode($rankTotals), '11111') !== false;
+        return $hasStraight;
+    }
+
+    // Return a sorted array of the distinct rank counts in a hand
+    // E.g. A hand with 1 A, 2 Ks, and 2 Qs returns [1,2,2]
+    private function getRankFrequencies() {
+        $rankTotals = array_fill_keys($this->cardRanks, 0);
+        foreach ($this->hand as &$card) { 
+            $rankTotals[str_split($card)[0]]++;   
+        }
+        $rankTotals = array_values(array_filter($rankTotals));
+        return $rankTotals;
+    }
+
 }
--- challenge-php/test/PokerHandTest.php
+++ challenge-php/test/PokerHandTest.php
@@ -41,5 +41,88 @@ class PokerHandTest extends TestCase
         $this->assertEquals('Flush', $hand->getRank());
     }
 
-    // TODO: More tests go here
+    /**
+     * @test
+     */
+    public function itCanRankAStraightFlush()
+    {
+        $hand = new PokerHand('3h 4h 5h 6h 7h');
+        $this->assertEquals('Straight Flush', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanRankAStraight()
+    {
+        $hand = new PokerHand('7h 4s 5h 6d 3s');
+        $this->assertEquals('Straight', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanRankThreeOfAKind()
+    {
+        $hand = new PokerHand('Kh Kc Ks 3h 2d');
+        $this->assertEquals('Three of a Kind', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanRankFourOfAKind()
+    {
+        $hand = new PokerHand('3h 3c 3s 3d 2d');
+        $this->assertEquals('Four of a Kind', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanRankAFullHouse()
+    {
+        $hand = new PokerHand('3h 3c 3s 2c 2d');
+        $this->assertEquals('Full House', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanRankAHighCard()
+    {
+        $hand = new PokerHand('3h Ac Js 2c 8d');
+        $this->assertEquals('High Card', $hand->getRank());
+    }
+     
+    /**
+     * @test
+     * Validation test
+     */
+    public function itWillThrowOnDuplicateCards() 
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $hand = new PokerHand('Kh 4h 4h 9s 10s');
+    }
+
+    /**
+     * @test
+     * Validation test
+     */
+    public function itWillThrowOnWrongNumberOfCards() 
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $hand = new PokerHand('Kh 4h 4d 9s 10s 8s 10d');
+    }
+
+    /**
+     * @test
+     * Validation test
+     */
+    public function itWillThrowOnInvalidCardString() 
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $hand = new PokerHand('9s 10s 8s 10d 99m');
+    }
+    
 }
